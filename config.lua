032bd10f780923c12565d4d9e03ba0ac79e9fff5
Brwnlee <77315942+ValiantCraven27@users.noreply.github.com>
2021-03-07 20:03:50 -0500

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,4 @@
+Config = {}
+
+Config.Key = 68 -- Weapon Wheel Key
+Config.NoWeapons = true -- turns off Player Driver Drive Bys only passengers can shoot
---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'NTRP SPEEDLIMITER'
+
+Author 'ValiantCraven27 | Adam Bravo'
+
+client_scripts {
+   'ntrp_speedlimiter.lua',
+   'config.lua'
+}
